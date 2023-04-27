77dcb8d24b8f73adbb6f3beae49baa205f52ad7f
ilya <zapaska96@gmail.com>
2023-04-27 16:19:50 +0500

    тени

--- client/src/canvas/CanvasModel.tsx
+++ client/src/canvas/CanvasModel.tsx
@@ -2,15 +2,21 @@ import { Canvas } from '@react-three/fiber';
 import { Environment, Center } from '@react-three/drei';
 import CameraRig from './CameraRig';
 import Shirt from './Shirt';
+import Backdrop from './Backdrop';
 
 const CanvasModel = () => {
     return (
-        <Canvas>
+        <Canvas
+            shadows
+            camera={{ position: [0, 0, 0], fov: 25 }}
+            gl={{ preserveDrawingBuffer: true }}
+            className='h-full w-full max-w-full transition-all ease-in'
+        >
             <ambientLight intensity={0.5} />
             <Environment preset='city' />
 
             <CameraRig>
-                {/* <BarProp /> */}
+                <Backdrop />
                 <Center>
                     <Shirt />
                 </Center>
